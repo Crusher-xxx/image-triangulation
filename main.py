a92c5997843a2356a158cf1e2325fe62761c4b7b
Danil Nikiforov <57360250+Crusher-xxx@users.noreply.github.com>
2024-09-04 00:23:28 +0300

    initialize project

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from ui.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    window = MainWindow()
+    window.resize(800, 600)
+    window.show()
+
+    sys.exit(app.exec())
---
+++ ui/main_window.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QMainWindow
+
+from ui.main_window_ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
