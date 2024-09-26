dbeb866972420bd14db58c515aa9c59167f91f68
Danil Nikiforov <57360250+Crusher-xxx@users.noreply.github.com>
2024-09-26 15:45:46 +0300

    tidy up

--- ui/graphicsview.py
+++ ui/graphicsview.py
@@ -12,29 +12,22 @@ class GraphicsView(QGraphicsView):
 
     def wheelEvent(self, event: QWheelEvent) -> None:
         angle = event.angleDelta().y()
-        zoom_factor = 1 + (angle/1000)
+        zoom_factor = 1 + angle / 1000
         self.scale(zoom_factor, zoom_factor)
         event.accept()
-        # return super().wheelEvent(event)
 
     def mousePressEvent(self, event: QMouseEvent) -> None:
         if event.button() == Qt.MouseButton.RightButton:
-            self.pan_start_x = event.x()
-            self.pan_start_y = event.y()
-
+            self._pan_start_x = event.position().x()
+            self._pan_start_y = event.position().y()
         return super().mousePressEvent(event)
-    
-    # def mouseReleaseEvent(self, event: QMouseEvent) -> None:
-    #     # if event.button() == Qt.MouseButton.RightButton:
-    #     #     self.rmb_pressed = False
-    #     return super().mouseReleaseEvent(event)
     
     def mouseMoveEvent(self, event: QMouseEvent) -> None:
         if event.buttons() is Qt.MouseButton.RightButton:
-            hsb_new = self.horizontalScrollBar().value() - (event.x() - self.pan_start_x)
-            vsb_new = self.verticalScrollBar().value() - (event.y() - self.pan_start_y)
+            hsb_new = self.horizontalScrollBar().value() - (event.position().x() - self._pan_start_x)
+            vsb_new = self.verticalScrollBar().value() - (event.position().y() - self._pan_start_y)
             self.horizontalScrollBar().setValue(hsb_new)
             self.verticalScrollBar().setValue(vsb_new)
-            self.pan_start_x = event.x()
-            self.pan_start_y = event.y()
+            self._pan_start_x = event.position().x()
+            self._pan_start_y = event.position().y()
         return super().mouseMoveEvent(event)
