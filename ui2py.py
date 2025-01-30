a4ba96fd8c9fc702b5e24c0998a1213feec97054
Danil Nikiforov <57360250+Crusher-xxx@users.noreply.github.com>
2025-01-30 11:19:45 +0700

    refactor

--- ui2py.py
+++ ui2py.py
@@ -10,4 +10,4 @@ py_dir.mkdir(exist_ok=True)
 ui_files = ui_dir.glob('*.ui')
 for ui_file in ui_files:
     py_file = py_dir / f'{ui_file.stem}.py'
-    subprocess.check_call(f'pyside6-uic {ui_file} -o {py_file}')
+    subprocess.run(f'pyside6-uic {ui_file} -o {py_file}')
